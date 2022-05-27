5d4b705a27600ad90ee7623dfb057b3e8f6390a2
Wojciech Koczwara <wojciech.koczwara@netguru.om>
2022-05-27 20:05:24 +0200

    initiall

---
+++ server/src/routes/planets/planets.router.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const { httpGetAllPlanets } = require("./planets.controllers");
+
+const planetsRouter = express.Router();
+
+planetsRouter.get("/", httpGetAllPlanets);
+
+module.exports = planetsRouter;
