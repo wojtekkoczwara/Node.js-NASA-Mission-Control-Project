f2cb60098da7790171ea65e91752d620c485d297
Wojciech Koczwara <61041670+wojtekkoczwara@users.noreply.github.com>
2022-05-27 20:13:10 +0200

    Delete server directory

--- server/src/routes/planets/planets.router.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { httpGetAllPlanets } = require("./planets.controllers");
-
-const planetsRouter = express.Router();
-
-planetsRouter.get("/", httpGetAllPlanets);
-
-module.exports = planetsRouter;
